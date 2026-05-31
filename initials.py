ed863e5862f2c3f3b118ec4deb48f0d3ae75d253
1mateen <2425403710@live.gctu.edu.gh>
2026-05-31 10:41:59 +0100

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: intials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Mensah Yaw Bewie Mateen
+# STUDENT ID: 2425403710
+# Grader: Augustus
+#
+# On my honor,Mensah Yaw Bewie Mateen, this programming assignment is my own work
+# and i have not provided this code to any other student.
+
+
 
 def main():
-  pass #code goes here
+     # Print small initials
+     print("...MYBM")
 
+     print()
 
+# Large stylized initials - M Y B M
+print("...MM......MM...YYYYYYYYYY...BBBBBBBBBB...MM......MM")
+print("...MMM....MMM...YYYYYYYYYY...BBBBBBBBBBB..MMM....MMM")
+print("...MMMM..MMMM.....YY..YY.....BB.......BB..MMMM..MMMM")
+print("...MM.MMMM.MM......YYYY......BB.......BB..MM.MMMM.MM")
+print("...MM..MM..MM.......YY.......BBBBBBBBBBB..MM..MM..MM")
+print("...MM......MM.......YY.......BBBBBBBBBB...MM......MM")
+print("...MM......MM.......YY.......BB.......BB..MM......MM")
+print("...MM......MM.......YY.......BB.......BB..MM......MM")
+print("...MM......MM...**..YY....**.BBBBBBBBBBB**MM......MM")
+print("...MM......MM...**..YY....**.BBBBBBBBBB.**MM......MM")
+
+print()
 
 main()
+
